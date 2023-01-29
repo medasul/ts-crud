fcdb8516dfd47c8fb4b9c81f283299316e33cb81
medasul <medasulskyte@gmail.com>
2023-01-29 21:38:43 +0200

    2.1 - constructor created

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,8 +1,21 @@
-type CarsCollectionProps = {
+import type Car from '../types/car';
+import type Model from '../types/model';
+import type Brand from '../types/brand';
 
+type CarsCollectionProps = {
+  cars: Car[],
+  brands: Brand[],
+  models: Model[],
 };
 
 class CarsCollection {
+ private props: CarsCollectionProps;
+
+ //Sukurkite konstruktorių, kuris priimtų markes, mašinas ir modelius. Gautus duomenis išsaugokite objekte
+  
+ constructor(props: CarsCollectionProps) {
+    this.props = props;
+  }
 
 }
 
